e7674877938c8417641a974edfa4f1f902ba4893
iCommunityR <erone47@gmail.com>
2026-05-24 10:33:33 +0300

    Delete chakulafoods directory

--- chakulafoods/src/app/wine-liquor-page/page.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import WineLiquorPageClient from '@/app/wine-liquor-page/components/WineLiquorPageClient';
-
-export default function WineLiquorPage() {
-  return <WineLiquorPageClient />;
-}
