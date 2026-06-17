e54fbf23fabeed60a622cc2bf0419a67688e252d
iCommunityR <erone47@gmail.com>
2026-06-17 23:45:48 +0300

    Delete chakulafoods directory

--- chakulafoods/src/app/confectionary-page/page.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ConfectionaryPageWrapper from '@/app/confectionary-page/components/ConfectionaryPageWrapper';
-
-export default function ConfectionaryPage() {
-  return <ConfectionaryPageWrapper />;
-}
--- chakulafoods/src/app/restaurant-page/page.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import RestaurantPageWrapper from '@/app/restaurant-page/components/RestaurantPageWrapper';
-
-export default function RestaurantPage() {
-  return <RestaurantPageWrapper />;
-}
