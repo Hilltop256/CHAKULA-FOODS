be77c2fa69dc12d3e75555c5d348d75fb7173f6c
iCommunityR <erone47@gmail.com>
2026-06-18 18:27:55 +0300

    Delete chakulafoods directory

--- chakulafoods/src/app/juice-bar-page/page.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import JuiceBarPageClient from '@/app/juice-bar-page/components/JuiceBarPageClient';
-
-export default function JuiceBarPage() {
-  return <JuiceBarPageClient />;
-}
