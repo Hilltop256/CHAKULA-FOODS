cf0bd8c682a51c9037e217924433c5905b117b31
iCommunityR <erone47@gmail.com>
2026-08-14 10:50:18 +0300

    Add files via upload

--- chakulafoods/src/app/page.tsx
+++ chakulafoods/src/app/page.tsx
@@ -3,14 +3,12 @@ import React from 'react';
 import HeroBanner from '@/app/components/HeroBanner';
 import DepartmentCards from '@/app/components/DepartmentCards';
 import HomePageClient from '@/app/components/HomePageClient';
-import TodaysOffers from '@/app/components/TodaysOffers';
 
 export default function HomePage() {
   return (
     <div className="min-h-screen bg-background">
       <HomePageClient>
         <HeroBanner />
-        <TodaysOffers />
         <DepartmentCards />
       </HomePageClient>
     </div>
